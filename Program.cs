448249d8bb4ef9ae72bef2a95f2bbf4acdbffc3c
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-05 20:43:30 +0100

    feat: implement basic crud methods

--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@ public class Program
         var service = new BookService(new BookContext());
 
         // Add a new BookAuthor relationship entry
-        var newEntry = new BookAuthor { BookId = 1, AuthorId = 3 };
+        // var newEntry = new BookAuthor { BookId = 1, AuthorId = 3 };
         // service.addBookAuthor(newEntry);
 
         // Get some books with its categories and all its authors
-        var books = service.getAllBooks();
+        // var books = service.getAllBooks();
         // foreach (var b in books)
         // {
         //     Console.WriteLine($"Book: {b.ID} - {b.Title}");
@@ -51,9 +51,59 @@ public class Program
         // Console.WriteLine(averagePublicationYear);
 
         // Add a new book and a new author, category is already added (non fiction id 6)
-        var taraWestover = service.addAuthor(new Author { FirstName = "Tara", LastName = "Westover" });
-        service.addBook(new Book() { Title = "Educated", CategoryId = 6, Authors = new List<Author> { taraWestover } });
+        // var taraWestover = service.addAuthor(new Author { FirstName = "Tara", LastName = "Westover" });
+        // service.addBook(new Book() { Title = "Educated", CategoryId = 6, Authors = new List<Author> { taraWestover } });
 
         // We intentionally left publication year as 0 above, so we can update it now
+        // service.updateBook(9, new Book() { PublicationYear = 2020 });
+
+        // Delete the newly created book
+        // service.deleteBook(9);
+
+        // Update an author
+        // service.updateAuthor(9, new Author { FirstName = "Tara", LastName = "Westover" });
+
+        // Delete an author
+        // service.deleteAuthor(9);
+
+        // Get authors and their books
+        // var authorsAndTheirBooks = service.getAllAuthors();
+        // foreach (var a in authorsAndTheirBooks)
+        // {
+        //     Console.WriteLine($"Firstname: {a.FirstName}, Lastname: {a.LastName}");
+
+        //     if (a.Books != null)
+        //     {
+        //         foreach (var b in a.Books)
+        //         {
+        //             Console.WriteLine($"Book: {b.Title}, {b.PublicationYear}, {b.Category?.Name}");
+        //         }
+        //     }
+        // }
+
+        // Find an author by its id
+        // var brandon = service.getAuthorById(3);
+        // Console.WriteLine(brandon?.FirstName);
+
+        // Find authors whose first name start with a certain string
+        // var match = service.getAuthorsWhereFirstNameStartsWith("b");
+        // var match = service.getAuthorsWhereLastNameEndWith("n");
+        // foreach (var a in match)
+        // {
+        //     Console.WriteLine($"{a.FirstName} {a.LastName}");
+        // }
+
+        // Get a category
+        // var cat = service.getCategoryById(1);
+        // Console.WriteLine(cat?.Name);
+
+        // Add a new category
+        // service.addCategory(new Category { Name = "New category" });
+
+        // Remove a category
+        // service.deleteCategory(9);
+
+        // Update a category
+        // service.updateCategory(8, new Category { Name = "Blæh" });
     }
 }
--- Services/BooksService.cs
+++ Services/BooksService.cs
@@ -13,11 +13,10 @@ public class BookService
         _ctx = ctx ?? throw new ArgumentNullException("DbContext is null");
     }
 
-    // Helper delegate methods
-
+    // Helper delegate method
     // Returns T (whatever the method using this has decided T is)
     // Takes one parameter cb of type Func<T> (Func<T> is a parameterless func that returns T)
-    private T ExecuteDb<T>(Func<T> cb)
+    private T Db<T>(Func<T> cb)
     {
         try
         {
@@ -30,25 +29,7 @@ public class BookService
         }
         catch (Exception e)
         {
-            Console.WriteLine("Something went wrong" + e.Message);
-            throw;
-        }
-    }
-
-    private void ExecuteDbVoid(Action cb)
-    {
-        try
-        {
-            cb();
-        }
-        catch (DbUpdateException e)
-        {
-            Console.WriteLine("Database operation failed: " + e.Message);
-            throw;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Something went wrong" + e.Message);
+            Console.WriteLine("Something went wrong: " + e.Message);
             throw;
         }
     }
@@ -56,17 +37,17 @@ public class BookService
     // - Reading from the database
 
     // Books table
-    public List<Book> getAllBooks() => ExecuteDb(() => _ctx.Books.Include(e => e.Authors).Include(e => e.Category).ToList());
+    public List<Book> getAllBooks() => Db(() => _ctx.Books.Include(e => e.Authors).Include(e => e.Category).ToList());
 
-    public Book getBookById(int id) => ExecuteDb(() => _ctx.Books.Where(e => e.ID == id).Single());
+    public Book getBookById(int id) => Db(() => _ctx.Books.Where(e => e.ID == id).Single());
 
-    public Book? getOldestBook() => ExecuteDb(() => _ctx.Books.OrderByDescending(e => e.PublicationYear).FirstOrDefault());
+    public Book? getOldestBook() => Db(() => _ctx.Books.OrderByDescending(e => e.PublicationYear).FirstOrDefault());
 
-    public List<Book> getBookInYearRange(int min, int max) => ExecuteDb(() => _ctx.Books.Where(e => e.PublicationYear > min && e.PublicationYear < max).ToList());
+    public List<Book> getBookInYearRange(int min, int max) => Db(() => _ctx.Books.Where(e => e.PublicationYear > min && e.PublicationYear < max).ToList());
 
-    public int getAveragePublicationYear() => ExecuteDb(() => (int)_ctx.Books.Average(e => e.PublicationYear));
+    public int getAveragePublicationYear() => Db(() => (int)_ctx.Books.Average(e => e.PublicationYear));
 
-    public void addBook(Book entry) => ExecuteDbVoid(() =>
+    public void addBook(Book entry) => Db(() =>
     {
         var exists = _ctx.Books.Any(e => e.Title.Trim().ToLower() == entry.Title.Trim().ToLower());
 
@@ -74,31 +55,83 @@ public class BookService
 
         _ctx.Books.Add(entry);
         _ctx.SaveChanges();
+
+        return true;
     });
 
-    public void updateBook() { }
-    public void deleteBook() { }
+    public void updateBook(int bookId, Book data) => Db(() =>
+    {
+        var book = _ctx.Books.Find(bookId);
+        if (book == null) throw new InvalidOperationException($"Unable to find book with id: {bookId}");
+
+        if (!string.IsNullOrWhiteSpace(data.Title)) book.Title = data.Title;
+        if (data.PublicationYear != 0) book.PublicationYear = data.PublicationYear;
+        if (data.CategoryId != 0) book.CategoryId = data.CategoryId;
+
+        _ctx.SaveChanges();
+
+        return true;
+    });
+
+    public void deleteBook(int bookId) => Db(() =>
+    {
+        var book = _ctx.Books.Find(bookId);
+        if (book == null) throw new InvalidOperationException($"Unable to find book with id: {bookId}");
+
+        _ctx.Books.Remove(book);
+        _ctx.SaveChanges();
+
+        return true;
+    });
 
     // Categories table
 
-    public void getAllCategories() { }
+    public List<Category> getAllCategories() => Db(() => _ctx.Categories.Include(e => e.Books).ToList());
 
-    public void getCategoryById(int id) { }
-    public void getBooksByCategory() { }
+    public Category? getCategoryById(int id) => Db(() => _ctx.Categories.Find(id));
 
-    public void addCategory(Category entry) { }
-    public void updateCategory() { }
-    public void deleteCategory() { }
+    public void addCategory(Category entry) => Db(() =>
+    {
+        var exists = _ctx.Categories.Any(e => e.Name == entry.Name);
+        if (exists) throw new InvalidOperationException("This category already exists");
+
+        _ctx.Categories.Add(entry);
+        _ctx.SaveChanges();
+
+        return true;
+    });
+
+    public void updateCategory(int id, Category data) => Db(() =>
+    {
+        var cat = _ctx.Categories.Find(id);
+        if (cat == null) throw new InvalidOperationException($"Unable to find category with id: {id}");
+
+        if (!string.IsNullOrWhiteSpace(data.Name)) cat.Name = data.Name;
+
+        _ctx.SaveChanges();
+
+        return true;
+    });
+
+    public void deleteCategory(int id) => Db(() =>
+    {
+        var cat = _ctx.Categories.Find(id);
+        if (cat == null) throw new InvalidOperationException($"Unable to find category with id: {id}");
+
+        _ctx.Categories.Remove(cat);
+        _ctx.SaveChanges();
+
+        return true;
+    });
 
     // Author table
 
-    public void getAllAuthors() { }
-    public void getAuthorById(int id) { }
-    public void getAuthorWhereFirstNameStartsWith() { }
-    public void getAuthorWhereLastNameStartsWith() { }
-    public void getBooksByAuthor() { }
+    public List<Author> getAllAuthors() => Db(() => _ctx.Authors.Include(e => e.Books).ToList());
+    public Author? getAuthorById(int id) => Db(() => _ctx.Authors.Find(id));
+    public List<Author> getAuthorsWhereFirstNameStartsWith(string pattern) => Db(() => _ctx.Authors.Where(e => EF.Functions.Like(e.FirstName, pattern + "%")).ToList());
+    public List<Author> getAuthorsWhereLastNameEndWith(string pattern) => Db(() => _ctx.Authors.Where(e => EF.Functions.Like(e.LastName, "%" + pattern)).ToList());
 
-    public Author addAuthor(Author entry) => ExecuteDb(() =>
+    public Author addAuthor(Author entry) => Db(() =>
     {
         var exists = _ctx.Authors.Any(e => e.FirstName.Trim().ToLower() == entry.FirstName.Trim().ToLower()
                                             && e.LastName.Trim().ToLower() == entry.LastName.Trim().ToLower());
@@ -111,11 +144,31 @@ public class BookService
         return entry;
     });
 
-    public void updateAuthor() { }
-    public void deleteAuthor() { }
+    public void updateAuthor(int authorId, Author data) => Db(() =>
+    {
+        var author = _ctx.Authors.Find(authorId);
+        if (author == null) throw new InvalidOperationException($"Unable to find author with id {authorId}");
+
+
+        if (!string.IsNullOrWhiteSpace(data.FirstName)) author.FirstName = data.FirstName;
+        if (!string.IsNullOrWhiteSpace(data.LastName)) author.LastName = data.LastName;
+
+        _ctx.SaveChanges();
+        return true;
+    });
+    public void deleteAuthor(int authorId) => Db(() =>
+    {
+        var author = _ctx.Authors.Find(authorId);
+        if (author == null) throw new InvalidOperationException($"Unable to find author with id {authorId}");
+
+        _ctx.Authors.Remove(author);
+        _ctx.SaveChanges();
+
+        return true;
+    });
 
     // BookAuthor table
-    public void addBookAuthor(BookAuthor entry) => ExecuteDbVoid(() =>
+    public void addBookAuthor(BookAuthor entry) => Db(() =>
         {
             // Check if the entry already exists
             var exists = _ctx.BookAuthor.Any(e => e.BookId == entry.BookId && e.AuthorId == entry.AuthorId);
@@ -123,6 +176,8 @@ public class BookService
 
             _ctx.BookAuthor.Add(entry);
             _ctx.SaveChanges();
+
+            return true;
         });
 
 }
