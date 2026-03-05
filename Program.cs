686a48fd08d5c372646e56db8e6cfd0b6c91ef79
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-05 14:06:11 +0100

    feat: implement addBook and addAuthor methods

--- Program.cs
+++ Program.cs
@@ -16,18 +16,44 @@ public class Program
 
         // Get some books with its categories and all its authors
         var books = service.getAllBooks();
-        foreach (var b in books)
-        {
-            Console.WriteLine($"Book: {b.ID} - {b.Title}");
-            Console.WriteLine($"Category {b.Category?.Name}");
-            Console.Write($"Authors: ");
-            foreach (var a in b.Authors)
-            {
-                Console.Write($"{a.FirstName} {a.LastName}");
-                if (b.Authors.Count > 1) Console.Write(", ");
-            }
-            Console.WriteLine("");
-            Console.WriteLine("");
-        }
+        // foreach (var b in books)
+        // {
+        //     Console.WriteLine($"Book: {b.ID} - {b.Title}");
+        //     Console.WriteLine($"Category {b.Category?.Name}");
+        //     Console.Write($"Authors: ");
+        //     foreach (var a in b.Authors)
+        //     {
+        //         Console.Write($"{a.FirstName} {a.LastName}");
+        //         if (b.Authors.Count > 1) Console.Write(", ");
+        //     }
+        //     Console.WriteLine("");
+        //     Console.WriteLine("");
+        // }
+
+        // Get a book by its ID
+        // var book = service.getBookById(1);
+        // Console.WriteLine($"{book.ID} - {book.Title} - {book.PublicationYear}");
+
+        // Get oldest book
+        // var oldest = service.getOldestBook();
+        // Console.WriteLine($"{oldest?.Title} - {oldest?.PublicationYear}");
+
+        // Get books in year range
+        // var betweenYears = service.getBookInYearRange(2000, 2020);
+        // foreach (var b in betweenYears)
+        // {
+        //     Console.WriteLine($"{b.ID} - {b.Title} {b.PublicationYear}");
+        // }
+
+        // Get averagePublicationYear
+        // var averagePublicationYear = service.getAveragePublicationYear();
+
+        // Console.WriteLine(averagePublicationYear);
+
+        // Add a new book and a new author, category is already added (non fiction id 6)
+        var taraWestover = service.addAuthor(new Author { FirstName = "Tara", LastName = "Westover" });
+        service.addBook(new Book() { Title = "Educated", CategoryId = 6, Authors = new List<Author> { taraWestover } });
+
+        // We intentionally left publication year as 0 above, so we can update it now
     }
 }
--- Services/BooksService.cs
+++ Services/BooksService.cs
@@ -13,70 +13,116 @@ public class BookService
         _ctx = ctx ?? throw new ArgumentNullException("DbContext is null");
     }
 
+    // Helper delegate methods
+
+    // Returns T (whatever the method using this has decided T is)
+    // Takes one parameter cb of type Func<T> (Func<T> is a parameterless func that returns T)
+    private T ExecuteDb<T>(Func<T> cb)
+    {
+        try
+        {
+            return cb();
+        }
+        catch (DbUpdateException e)
+        {
+            Console.WriteLine("Database operation failed: " + e.Message);
+            throw;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something went wrong" + e.Message);
+            throw;
+        }
+    }
+
+    private void ExecuteDbVoid(Action cb)
+    {
+        try
+        {
+            cb();
+        }
+        catch (DbUpdateException e)
+        {
+            Console.WriteLine("Database operation failed: " + e.Message);
+            throw;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something went wrong" + e.Message);
+            throw;
+        }
+    }
+
     // - Reading from the database
 
     // Books table
-    public List<Book> getAllBooks()
+    public List<Book> getAllBooks() => ExecuteDb(() => _ctx.Books.Include(e => e.Authors).Include(e => e.Category).ToList());
+
+    public Book getBookById(int id) => ExecuteDb(() => _ctx.Books.Where(e => e.ID == id).Single());
+
+    public Book? getOldestBook() => ExecuteDb(() => _ctx.Books.OrderByDescending(e => e.PublicationYear).FirstOrDefault());
+
+    public List<Book> getBookInYearRange(int min, int max) => ExecuteDb(() => _ctx.Books.Where(e => e.PublicationYear > min && e.PublicationYear < max).ToList());
+
+    public int getAveragePublicationYear() => ExecuteDb(() => (int)_ctx.Books.Average(e => e.PublicationYear));
+
+    public void addBook(Book entry) => ExecuteDbVoid(() =>
     {
-        try
-        {
-            return _ctx.Books.Include(e => e.Authors).Include(e => e.Category).ToList();
-        }
-        catch (DbUpdateException e)
-        {
-            Console.WriteLine("Unable to save to the database", e.Message);
-            throw;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Something went wrong", e.Message);
-            throw;
-        }
-    }
+        var exists = _ctx.Books.Any(e => e.Title.Trim().ToLower() == entry.Title.Trim().ToLower());
 
-    public void getBookById(int id) { }
+        if (exists) throw new InvalidOperationException("This book already exists");
 
+        _ctx.Books.Add(entry);
+        _ctx.SaveChanges();
+    });
+
+    public void updateBook() { }
+    public void deleteBook() { }
+
+    // Categories table
+
+    public void getAllCategories() { }
+
+    public void getCategoryById(int id) { }
     public void getBooksByCategory() { }
 
+    public void addCategory(Category entry) { }
+    public void updateCategory() { }
+    public void deleteCategory() { }
+
+    // Author table
+
+    public void getAllAuthors() { }
+    public void getAuthorById(int id) { }
+    public void getAuthorWhereFirstNameStartsWith() { }
+    public void getAuthorWhereLastNameStartsWith() { }
     public void getBooksByAuthor() { }
 
-    public void getOldestBook() { }
+    public Author addAuthor(Author entry) => ExecuteDb(() =>
+    {
+        var exists = _ctx.Authors.Any(e => e.FirstName.Trim().ToLower() == entry.FirstName.Trim().ToLower()
+                                            && e.LastName.Trim().ToLower() == entry.LastName.Trim().ToLower());
 
-    public void getBookInYearRange() { }
+        if (exists) throw new InvalidOperationException("This author exists already");
 
-    public void getAveragePublicationYear() { }
+        var newEntry = _ctx.Authors.Add(entry);
+        _ctx.SaveChanges();
 
-    // Categories table
+        return entry;
+    });
 
-    // ---- Inserting to the database
-
-    // Books table
-
-    // Categories table
+    public void updateAuthor() { }
+    public void deleteAuthor() { }
 
     // BookAuthor table
-    public void addBookAuthor(BookAuthor data)
-    {
-        try
+    public void addBookAuthor(BookAuthor entry) => ExecuteDbVoid(() =>
         {
             // Check if the entry already exists
-            var exists = _ctx.BookAuthor.Any(e => e.BookId == data.BookId && e.AuthorId == data.AuthorId);
+            var exists = _ctx.BookAuthor.Any(e => e.BookId == entry.BookId && e.AuthorId == entry.AuthorId);
             if (exists) throw new InvalidOperationException("This entry already exists");
 
-            _ctx.BookAuthor.Add(data);
+            _ctx.BookAuthor.Add(entry);
             _ctx.SaveChanges();
-        }
-        catch (DbUpdateException e)
-        {
-            Console.WriteLine("Unable to update database", e.Message);
-            throw;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Something went wrong", e.Message);
-            throw;
-        }
+        });
 
-
-    }
 }
